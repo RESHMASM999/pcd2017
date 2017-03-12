680ce76643d384e58c276503d1ad1b57b4300811
RESHMASM999 <resh16cs@cmrit.ac.in>
2017-03-12 13:44:17 +0530

    Create rec.c

---
+++ rec.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int findarea(int a,int b)
+{
+int c;
+c=a*b;
+return c;
+}
+int main()
+{
+printf("enter the values of a and b"a,b);
+scanf(("%d,%d",&a,&b);
+area(a,b);
+printf("area of a rectangle:%d",c);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
