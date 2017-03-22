77853588fc5573d33ff38a99f1b2d1ff24739ee8
RESHMASM999 <resh16cs@cmrit.ac.in>
2017-03-22 14:16:20 +0530

    Create tay.c

---
+++ tay.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int com(int a[],int x,int n);
+{
+int sum=0;
+for(int i=n;i>0;i--)
+{
+sum=(sum+a[0]*x;
+}
+sum=sum+a[0];
+return sum;
+}
+
+int main()
+{
+int n,x;
+int a[n+1];
+printf("enter the order");
+scanf("%d",&n);
+for(int i=0;i<=n;i++)
+{
+scanf("%d"&a[i]);
+scanf("%d",&x);
+}
+int res=com(a,x.n);
+printf("the sum is %d",sum);
+}
+
+
